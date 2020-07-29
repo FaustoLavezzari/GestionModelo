c3aac885aefab4bed6818a4cb793e26bf54ceb64
Fausto Lavezzari <faustolavezzari99@gmail.com>
2020-07-29 17:45:22 -0300

    me quedo un metodo de mas je

--- Menu_Principal.cs
+++ Menu_Principal.cs
@@ -70,11 +70,6 @@ namespace GestiónModelo
             AbrirFormHija3(new asistencia(sesion.getDiccionarioPaises()));
         }
 
-        private void terminarPrograma(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
         private void terminarPrograma(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
