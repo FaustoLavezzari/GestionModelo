786975bbffc741d59514226406d5eb3da0a2d310
Fausto Lavezzari <faustolavezzari99@gmail.com>
2020-08-04 14:59:30 -0300

    Revert "Refactor InfoDelegacion ahora es un panel mas accesible y puede interactuar mejor con el programa"
    This reverts commit b5fee2f2617ef280d0591d030a16b9d1d1848b75.

---
+++ InfoDelegacion.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace GestiónModelo
+{
+    partial class InfoDelegacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fefefe = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Discurso = new System.Windows.Forms.Label();
+            this.Bandera = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Asistencia = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.InterpelacionesRealizadas = new System.Windows.Forms.Label();
+            this.InterpelacionesRespondidas = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.subir_est = new System.Windows.Forms.Button();
+            this.btn_discurso = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Bandera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fefefe
+            // 
+            this.fefefe.AutoSize = true;
+            this.fefefe.Location = new System.Drawing.Point(69, 74);
+            this.fefefe.Name = "fefefe";
+            this.fefefe.Size = new System.Drawing.Size(47, 13);
+            this.fefefe.TabIndex = 0;
+            this.fefefe.Text = "Nombre:";
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Location = new System.Drawing.Point(122, 74);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(119, 13);
+            this.Nombre.TabIndex = 1;
+            this.Nombre.Text = "Aca se carga el nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Discurso:";
+            // 
+            // Discurso
+            // 
+            this.Discurso.AutoSize = true;
+            this.Discurso.Location = new System.Drawing.Point(122, 135);
+            this.Discurso.Name = "Discurso";
+            this.Discurso.Size = new System.Drawing.Size(71, 13);
+            this.Discurso.TabIndex = 3;
+            this.Discurso.Text = "Leido/sin leer";
+            // 
+            // Bandera
+            // 
+            this.Bandera.Location = new System.Drawing.Point(428, 40);
+            this.Bandera.Name = "Bandera";
+            this.Bandera.Size = new System.Drawing.Size(187, 126);
+            this.Bandera.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Bandera.TabIndex = 4;
+            this.Bandera.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Asistencia:";
+            // 
+            // Asistencia
+            // 
+            this.Asistencia.AutoSize = true;
+            this.Asistencia.Location = new System.Drawing.Point(136, 107);
+            this.Asistencia.Name = "Asistencia";
+            this.Asistencia.Size = new System.Drawing.Size(93, 13);
+            this.Asistencia.TabIndex = 6;
+            this.Asistencia.Text = "Presente/Ausente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Interpelaciones:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(125, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Realizadas:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(125, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Respondidas:";
+            // 
+            // InterpelacionesRealizadas
+            // 
+            this.InterpelacionesRealizadas.AutoSize = true;
+            this.InterpelacionesRealizadas.Location = new System.Drawing.Point(194, 201);
+            this.InterpelacionesRealizadas.Name = "InterpelacionesRealizadas";
+            this.InterpelacionesRealizadas.Size = new System.Drawing.Size(49, 13);
+            this.InterpelacionesRealizadas.TabIndex = 10;
+            this.InterpelacionesRealizadas.Text = "Cantidad";
+            // 
+            // InterpelacionesRespondidas
+            // 
+            this.InterpelacionesRespondidas.AutoSize = true;
+            this.InterpelacionesRespondidas.Location = new System.Drawing.Point(207, 230);
+            this.InterpelacionesRespondidas.Name = "InterpelacionesRespondidas";
+            this.InterpelacionesRespondidas.Size = new System.Drawing.Size(49, 13);
+            this.InterpelacionesRespondidas.TabIndex = 11;
+            this.InterpelacionesRespondidas.Text = "Cantidad";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(428, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 41);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Valorar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(75, 346);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Valoraciones";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(78, 362);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(537, 250);
+            this.listView1.TabIndex = 16;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // subir_est
+            // 
+            this.subir_est.Location = new System.Drawing.Point(428, 172);
+            this.subir_est.Name = "subir_est";
+            this.subir_est.Size = new System.Drawing.Size(187, 42);
+            this.subir_est.TabIndex = 17;
+            this.subir_est.Text = "Subir Al Estrado";
+            this.subir_est.UseVisualStyleBackColor = true;
+            this.subir_est.Click += new System.EventHandler(this.subir_est_Click);
+            // 
+            // btn_discurso
+            // 
+            this.btn_discurso.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_discurso.Location = new System.Drawing.Point(347, 315);
+            this.btn_discurso.Name = "btn_discurso";
+            this.btn_discurso.Size = new System.Drawing.Size(75, 41);
+            this.btn_discurso.TabIndex = 18;
+            this.btn_discurso.Text = "Marcar Discurso";
+            this.btn_discurso.UseVisualStyleBackColor = false;
+            this.btn_discurso.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InfoDelegacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 663);
+            this.Controls.Add(this.btn_discurso);
+            this.Controls.Add(this.subir_est);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.InterpelacionesRespondidas);
+            this.Controls.Add(this.InterpelacionesRealizadas);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Asistencia);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Bandera);
+            this.Controls.Add(this.Discurso);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.fefefe);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "InfoDelegacion";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.InfoDelegacion_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Bandera)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fefefe;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Discurso;
+        private System.Windows.Forms.PictureBox Bandera;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Asistencia;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label InterpelacionesRealizadas;
+        private System.Windows.Forms.Label InterpelacionesRespondidas;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button subir_est;
+        private System.Windows.Forms.Button btn_discurso;
+    }
+}
---
+++ InfoDelegacion.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestiónModelo
+{
+    public partial class InfoDelegacion : Form
+    {
+        
+        private Delegacion delegacion;   
+        private Topico topico_activo;
+        private Panel panel_Est;
+        private Sesion sesion;
+        private PantallaPrincipal pant_p;
+        public InfoDelegacion(Delegacion delegacion, Topico topico_activo, Panel panel_estrado, Sesion sesion, PantallaPrincipal pp )
+        {
+            InitializeComponent();
+            panel_Est = panel_estrado;
+            this.delegacion = delegacion;
+            this.topico_activo = topico_activo;
+            this.sesion = sesion;
+            pant_p = pp;
+        }
+
+        private void InfoDelegacion_Load(object sender, EventArgs e)
+        {
+            Bandera.Image =(Image) GestiónModelo.BanderasGrandes.ResourceManager.GetObject(delegacion.getNombre() + "G");
+            Nombre.Text = delegacion.getNombre();
+            
+            if (delegacion.getAsistencia())
+                Asistencia.Text = "Presente";
+            else
+                Asistencia.Text = "Ausente";
+
+            if (topico_activo.leyoDiscurso(delegacion))
+                Discurso.Text = "Leido";
+            else
+                Discurso.Text = "Sin leer";
+           
+            InterpelacionesRealizadas.Text =  delegacion.getInterpelaciones().ToString();
+            InterpelacionesRespondidas.Text = delegacion.getPregResp().ToString();
+        }
+        private void AbrirFormHija(object form_hija)
+        {
+            if (this.panel_Est.Controls.Count > 0)
+                this.panel_Est.Controls.RemoveAt(0);
+            Form del_est = form_hija as Form;
+            del_est.TopLevel = false;
+            del_est.Dock = DockStyle.Fill;
+            this.panel_Est.Controls.Add(del_est);
+            this.panel_Est.Tag = del_est;
+            del_est.Show();
+        }
+        private void subir_est_Click(object sender, EventArgs e)
+        {
+            Del_Estrado del_est = new Del_Estrado();
+            del_est.CargarDelegacion(delegacion);
+            AbrirFormHija(del_est);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (btn_discurso.Text == "Marcar Discurso")
+            {
+                sesion.getTopicoActivo().discursosLeidos().Add(delegacion);
+                btn_discurso.Text = "Desmarcar Discurso";
+                btn_discurso.BackColor = Color.YellowGreen;
+                pant_p.comboBox1_SelectedIndexChanged(sender, e);
+            }
+            else {
+                sesion.getTopicoActivo().discursosLeidos().Remove(delegacion);
+                btn_discurso.Text = "Marcar Discurso";
+                btn_discurso.BackColor = DefaultBackColor;
+                pant_p.comboBox1_SelectedIndexChanged(sender, e);
+            }
+        }
+    }
+}
